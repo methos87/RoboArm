48cc0d672e916f0dd77ec327a26187ef7e7349ee
methos87 <novakakos87@gmail.com>
2023-12-09 09:14:55 +0100

    update 20231209

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('robo_arm'))
-    xacro_file = os.path.join(pkg_path,'urdf','robot_assembly(gazebo).xacro')
+    xacro_file = os.path.join(pkg_path,'urdf','robot_core.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
